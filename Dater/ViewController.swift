2d4a3ddaf892c10099b86ad1f602d3e8d6945087
Devesh Nema <deveshnema@deveshs-macbook-air.local>
2018-05-25 23:41:08 -0700

    Initial Commit

---
+++ Dater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dater
+//
+//  Created by Devesh Nema on 5/25/18.
+//  Copyright © 2018 Devesh Nema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
